a13eff1c36d5385bfb0836db0e80d00d766e2000
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-11-14 15:30:35 -0300

    Install Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap/dist/js/bootstrap.bundle.min
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
