b3ecb88202975d6d47110df33cc0f963c69e2f68
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-11-15 18:56:58 -0300

    Reconfigure root route depending on authentication and translate user email templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # resources :users, only: :confirm_email
-
-  root 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root to: 'home#index'
+    end
+    
+    unauthenticated do
+      root to: 'devise/sessions#new'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
