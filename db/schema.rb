33bfdf6b0c4d6e9514f98254420eaa91a22f18c2
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-11-16 16:40:10 -0300

    Establish relation between appointments and services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_191759) do
+ActiveRecord::Schema.define(version: 2020_11_16_193253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_11_16_191759) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "service_id"
+    t.index ["service_id"], name: "index_appointments_on_service_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
 
@@ -72,5 +74,6 @@ ActiveRecord::Schema.define(version: 2020_11_16_191759) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "appointments", "services"
   add_foreign_key "appointments", "users"
 end
