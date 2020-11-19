b3fcb5c275b544423a8f8edb12e6f5aa95a39ad5
Victor Gutierrez Diban <vgutierrezdiban@protonmail.com>
2020-11-19 00:05:53 -0300

    Remove comments tab from Active Admin

--- app/admin/appointment.rb
+++ app/admin/appointment.rb
@@ -1,8 +1,14 @@
 ActiveAdmin.register Appointment do
   index do
+    selectable_column
     column "Fecha", :start
     column "Nombre del cliente", :client_name
     column "Tipo de servicio", :service_name
     column "Número de teléfono del cliente", :client_phone_number
   end
+
+  filter :start
+  filter :user
+  filter :service
+
 end
